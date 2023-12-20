ebf622636b66ffefd2042abd751d544b55ad9ade
Cedric Mailleux <cmailleux@jahia.com>
2023-12-20 12:08:07 -0500

    BACKLOG-21378: Simplify code before making it community

--- src/main/java/org/jahia/modules/channels/filters/ChannelExclusionFilter.java
+++ src/main/java/org/jahia/modules/channels/filters/ChannelExclusionFilter.java
@@ -52,14 +52,16 @@ import org.jahia.services.render.filter.RenderChain;
 import org.slf4j.Logger;
 
 import javax.jcr.Property;
+import javax.jcr.RepositoryException;
 import javax.jcr.Value;
+import java.util.Objects;
 
 /**
  * This filter will exclude a module rendering from a node if it has the associated configuration.
  */
 public class ChannelExclusionFilter extends AbstractFilter {
 
-    protected transient static Logger logger = org.slf4j.LoggerFactory.getLogger(ChannelExclusionFilter.class);
+    protected static Logger logger = org.slf4j.LoggerFactory.getLogger(ChannelExclusionFilter.class);
 
     private ChannelService channelService;
 
@@ -72,27 +74,10 @@ public class ChannelExclusionFilter extends AbstractFilter {
         if (resource.getNode().isNodeType("jmix:channelSelection")) {
             // we have a mixin applied, let's test if we must exclude it from the current channel.
             if (!resource.getNode().hasProperty("j:channelSelection")) {
-                logger.warn("Channel selection activated on resource "+resource.getPath()+" but no channels selected, please select channels for this function to be activated properly.");
+                logger.warn("Channel selection activated on resource {} but no channels selected, please select channels for this function to be activated properly.", resource.getPath());
                 return null;
             }
-            Property channelExclusionProperty = resource.getNode().getProperty("j:channelSelection");
-            String includeOrExclude = resource.getNode().hasProperty("j:channelIncludeOrExclude") ? resource.getNode().getProperty("j:channelIncludeOrExclude").getString() : "exclude";
-            Value[] channelExclusionValues = channelExclusionProperty.getValues();
-            Channel currentChannel = renderContext.getChannel();
-            for (Value channelExclusionValue : channelExclusionValues) {
-                if (channelExclusionValue.getString() != null) {
-                    boolean inList = channelService.matchChannel(channelExclusionValue.getString(), currentChannel);
-                    if (inList && includeOrExclude.equals("exclude")) {
-                        return getExcludedResult(resource, renderContext);
-                    }
-                    if (inList && includeOrExclude.equals("include")) {
-                        return null;
-                    }
-                }
-            }
-            if (includeOrExclude.equals("include")) {
-                return getExcludedResult(resource, renderContext);
-            }
+            return filterChannel(resource, renderContext);
         }
         return null;
     }
@@ -107,4 +92,29 @@ public class ChannelExclusionFilter extends AbstractFilter {
         return null;
     }
 
+    private String filterChannel(Resource resource, RenderContext renderContext) throws RepositoryException {
+        boolean isExcluding = isExcluding(resource);
+        Value[] channelExclusionValues = getChannelExclusionValues(resource);
+        Channel currentChannel = renderContext.getChannel();
+        for (Value channelExclusionValue : channelExclusionValues) {
+            String exclusionValueString = channelExclusionValue.getString();
+            if (exclusionValueString != null && channelService.matchChannel(exclusionValueString, currentChannel)) {
+                return isExcluding ? getExcludedResult(resource, renderContext) : null;
+            }
+        }
+        if (!isExcluding) {
+            return getExcludedResult(resource, renderContext);
+        }
+        return null;
+    }
+
+    private static boolean isExcluding(Resource resource) throws RepositoryException {
+        String channelRule = resource.getNode().hasProperty("j:channelIncludeOrExclude") ? resource.getNode().getProperty("j:channelIncludeOrExclude").getString() : "exclude";
+        return channelRule.equalsIgnoreCase("exclude");
+    }
+
+    private static Value[] getChannelExclusionValues(Resource resource) throws RepositoryException {
+        Property channelExclusionProperty = resource.getNode().getProperty("j:channelSelection");
+        return channelExclusionProperty.getValues();
+    }
 }
